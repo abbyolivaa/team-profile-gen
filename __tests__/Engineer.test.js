fb4c004c8ac530106964ea1d0b6e14cebe36a605
Abby Oliva <abbyoliva6@icloud.com>
2022-04-30 13:20:04 -0600

    engineer tests and js completed

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,8 +1,23 @@
-const Employee = require('../lib/Engineer');
 const Engineer = require('../lib/Engineer')
 
-test ('github username')
+test ('engineer info', () => {
+    const engineer = new Engineer('Quin', '3', 'quin12@gmail.com')
 
-test('gets github info')
+    expect(engineer.name).toBe('Quin');
+    expect(engineer.id).toBe('3');
+    expect(engineer.email).toBe('quin12@gmail.com');
+})
 
-test ('gets role')
+test("gets engineer's github username", () => {
+    const engineer = new Engineer('Quin', '3', 'quin12@gmail.com', 'quin12');
+    
+    expect(engineer.github).toBe('quin12');
+    expect(engineer.getGithub()).toBe('quin12')
+
+})
+
+
+
+//test('gets github info')
+
+//test ('gets role')
--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -9,5 +9,8 @@ test("gets manager's info", () => {
     expect(manager.officeNum).toEqual('311');
 })
 
+test ('get role', () => {
+    const manager = new Manager ('Ana', '2', 'ana123@gmail.com','311')
 
-
+    expect(manager.getRole()).toBe ('Manager')
+})
