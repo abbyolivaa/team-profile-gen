319858aa473d0672bd38ef72a106cec873a0279e
Abby Oliva <abbyoliva6@icloud.com>
2022-04-30 12:59:34 -0600

    manager tests and js completed

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,12 +1,13 @@
 const Manager = require('../lib/Manager');
 
 test("gets manager's info", () => {
-    const manager = new Manager ('Ana', '2', 'ana123@gmail.com')
+    const manager = new Manager ('Ana', '2', 'ana123@gmail.com','311')
 
-    expect(manager.name).toBe('Ana')
-    expect(manager.id).toBe('2')
-    expect(manager.email).toBe('ana123@gmail.com')
+    expect(manager.name).toBe('Ana');
+    expect(manager.id).toBe('2');
+    expect(manager.email).toBe('ana123@gmail.com');
+    expect(manager.officeNum).toEqual('311');
 })
 
-test ("gets manager's office number")
+
 
